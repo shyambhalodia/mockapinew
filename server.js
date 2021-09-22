5e1e2f0ca4dc7cc24fa3822acdc65fa7568196a1
ShyamBhalodia <shyambtridhyatech@gmail.com>
2021-09-22 16:53:15 +0530

    new change

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-import { create, router as _router, defaults, rewriter } from "json-server";
-const server = create();
-const router = _router("db.json");
-// const path = require("path");
-// const express = require("express");
-const middlewares = defaults({
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
   static: "./build",
 });
-
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 server.use(middlewares);
 server.use(
-  rewriter({
+  jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
-
 server.use(router);
-
-// app.use("/db", middlewares, router);
-// app.use(express.static(path.join(__dirname, "build")));
-
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
-
-server.listen(port, () => {
-  console.log("server is running");
+server.listen(PORT, () => {
+  console.log("Server is running");
 });
